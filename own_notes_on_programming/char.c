53686f0fba0f646df3fe1cc0ad820d793cc49566
Makena-Wambui <iammakena@gmail.com>
2023-08-21 12:43:08 +0000

    How to use printf,puts and putchar

---
+++ own_notes_on_programming/char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	putchar('@');
+	putchar(' ');
+	putchar('*');
+	int q;
+	for(q=0;q<5;q++)
+	{	
+		putchar('$');
+	}
+       putchar('\n');
+}
---
+++ own_notes_on_programming/poots.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	char *day=("What is the day today?");
+	puts(day);
+	scanf("%s",day);
+}
